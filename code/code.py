6c10a1d65cdf35b53c40c0e173b30c106d226020
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-01-31 18:59:42 +0100

    update code.py

--- code/code.py
+++ code/code.py
@@ -12,7 +12,11 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LinearRegression
-from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from math import trunc
+from sklearn import tree
 
 ###################################################################
 ########## Charge le dataset dans la variable dataset #############
@@ -83,22 +87,78 @@ for name, values in dataset_df_reduced_rows.iteritems():
 ########################### Entrainement ##########################
 ###################################################################
 
-# print(dataset_copy)
-# print(dict_init)
+dataset_df_reduced_rows = dataset_df_reduced_rows.sample(random_state=0, n=dataset_df_reduced_rows.shape[0])
+X = dataset_df_reduced_rows.drop(columns=['target'])
+y = dataset_df_reduced_rows['target']
 
-# simple
+#print(X.info())
 
-X_train, X_test, y_train, y_test = train_test_split(
-    dataset_df_reduced_rows.drop(columns=['target']), dataset_df_reduced_rows['target'], random_state=0)
+# Simple
 
-# lr = LinearRegression().fit(X_train,y_train)
-lr = MLPRegressor(random_state=1, max_iter=500).fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-# print("Training set score: {:.2f}".format(lr.score(X_train,y_train)))
-# print("Test set score: {:.2f}".format(lr.score(X_test,y_test)))
+scalar = StandardScaler()
+X_train = scalar.fit_transform(X_train)
+X_test = scalar.fit_transform(X_test)
+
+lr = LinearRegression().fit(X_train,y_train)
+
+print("Training set score: {:.2f}".format(lr.score(X_train,y_train)))
+print("Test set score: {:.2f}".format(lr.score(X_test,y_test)))
 
 # plus complexe
 
-# scores = cross_val_score(lr, X_test, y_test, cv=20)
-# print(scores)
-print(lr.score(X_test, y_test))
+pipeline = Pipeline([('transformer', scalar), ('estimator', LinearRegression())])
+
+scores = cross_val_score(pipeline, X, y, cv=20)
+print(scores)
+print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+# print(lr.score(X_test, y_test))
+
+pipeline = Pipeline([('transformer', scalar), ('estimator', Ridge(alpha=1.0))])
+
+scores = cross_val_score(pipeline, X, y, cv=20)
+print(scores)
+print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+
+#clf = svm.SVC(kernel='linear', C=1, random_state=42)
+#scores = cross_val_score(clf, X, y, cv=5)
+#print(scores)
+
+###################################################################
+###### Traitement pour passer le problème en classification #######
+###################################################################
+
+# pas de l'intervale des fps. Ex : si pas = 10, alors de 0 à 10 fps, y deviendra 0, de 11 à 20, y deviendra 1
+pas = 10
+
+dataset_copy = dataset_df_reduced_rows.copy()
+
+print(dataset_copy['target'])
+
+for j in range(len(dataset_copy['target'])):
+    dataset_copy['target'][j] = trunc(dataset_copy['target'][j] / pas)
+
+dataset_copy = dataset_copy.astype({'target': 'int32'})
+print(dataset_copy['target'])
+
+##################################################################
+####### Application arbre décision / régression logistique #######
+##################################################################
+
+X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(dataset_copy.drop(columns=['target']), dataset_copy['target'], test_size=0.33)
+
+scalar = StandardScaler()
+X_train_c = scalar.fit_transform(X_train_c)
+X_test_c = scalar.fit_transform(X_test_c)
+
+# clf = LogisticRegression(solver='lbfgs', max_iter=2000)
+clf = tree.DecisionTreeClassifier(random_state=0, max_depth=11)
+
+clf.fit(X_train_c, y_train_c)
+
+train_score_c = clf.score(X_train_c, y_train_c)
+test_score_c = clf.score(X_test_c, y_test_c)
+
+print("Training set score: {:.2f} ".format(train_score_c))
+print("Test set score: {:.2f} ".format(test_score_c))
